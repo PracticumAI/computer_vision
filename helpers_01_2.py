967c5d32c1e1d1f9e1d229038a7da4b5adcc3cc4
Matt Gitzendanner <magitz@ufl.edu>
2024-07-08 20:51:58 -0400

    Finishing clean versions, clear outputs

--- helpers_01_2.py
+++ helpers_01_2.py
@@ -4,180 +4,153 @@ import time
 import tarfile
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+import datetime
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-def download_file(url="https://www.dropbox.com/s/x70hm8mxqhe7fa6/bee_vs_wasp.tar.gz?dl=1", filename="bee_vs_wasp.tar.gz"):
+from tensorflow.keras.models import Sequential  # Import the Sequential model: a linear stack of layers from Keras module in TensorFlow.
+from tensorflow.keras.layers import Dense  # Import the Dense layer: a fully connected neural network layer from Keras module in TensorFlow.
+from tensorflow.keras.layers import Flatten  # Import the Flatten layer: used to convert input data into a 1D array from Keras module in TensorFlow.
+from tensorflow.keras.losses import SparseCategoricalCrossentropy  # Import the SparseCategoricalCrossentropy loss function from Keras module in TensorFlow.
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers 
+from tensorflow.keras import losses
+from sklearn.metrics import confusion_matrix 
 
-    # Download the file using requests
-    response = requests.get(url, stream=True)
-
-    # Create a file object and write the response content in chunks
-    with open(filename, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-    # Wait for the file to finish downloading
-    while not os.path.exists(filename):
-        time.sleep(1)
-
-    # Print a success message
-    print(f"Downloaded {filename} successfully.")
-
-def extract_file(filename, data_folder):
-    # Check if the file is a tar file
-    if tarfile.is_tarfile(filename):
-        # Open the tar file
-        tar = tarfile.open(filename, "r:gz")
-        # Extract all the files to the data folder, filter for security
-        tar.extractall(data_folder, filter='data')
-        # Close the tar file
-        tar.close()
-        # Print a success message
-        print(f"Extracted {filename} to {data_folder} successfully.")
-    else:
-        # Print an error message
-        print(f"{filename} is not a valid tar file.")
-    
-def manage_data(url="https://www.dropbox.com/s/x70hm8mxqhe7fa6/bee_vs_wasp.tar.gz?dl=1", filename="bee_vs_wasp.tar.gz", folder_name='bee_vs_wasp', dest='data'):
-    '''Try to find the data for the exercise and return the path'''
-    
-    # Check common paths of where the data might be on different systems
-    likely_paths= [os.path.normpath(f'/blue/practicum-ai/share/data/{folder_name}'),
-                   os.path.normpath(f'/project/scinet_workshop2/data/{folder_name}'),
-                   os.path.join('data', folder_name),
-                   os.path.normpath(folder_name)]
-    
-    for path in likely_paths:
-        if os.path.exists(path):
-            print(f'Found data at {path}.')
-            return path
-
-    answer = input(f'Could not find data in the common locations. Do you know the path? (yes/no): ')
-
-    if answer.lower() == 'yes':
-        path = os.path.join(os.path.normpath(input('Please enter the path to the data folder: ')),folder_name)
-        if os.path.exists(path):
-            print(f'Thanks! Found your data at {path}.')
-            return path
-        else:
-            print(f'Sorry, that path does not exist.')
-    
-    answer = input('Do you want to download the data? (yes/no): ')
-
-    if answer.lower() == 'yes':
-        print('Downloading data, this may take a minute.')
-        download_file()
-        print('Data downloaded, unpacking')
-        extract_file(filename, dest)
-        print('Data downloaded and unpacked. Now available at {os.path.join(data,folder_name)}.')
-        return os.path.normpath(os.path.join(data,folder_name))   
-
-    print('Sorry, I cannot find the data. Please download it manually from https://www.dropbox.com/s/x70hm8mxqhe7fa6/bee_vs_wasp.tar.gz and unpack it to the data folder.')      
+from sklearn.utils import class_weight
 
 
-def count_class(counts, batch, classes):
-    '''Count number of samples per class in a batch'''
-    for i in range(classes):
-        cc = tf.cast(batch[1] == i, tf.int32)
-        counts[i] += tf.reduce_sum(cc)
-
-
-def load_display_data(path, batch_size=32, shape=(80,80,3), show_pictures=True, stratify=False, return_cls_counts=False):
-    '''Takes a path, batch size, target shape for images and optionally whether to show sample images.
-       Returns training and testing datasets
+def load_display_data(path, batch_size=32, shape=(80,80,3), show_pictures=True,
+                      stratify=True, return_cls_counts=False):
+    '''Takes info on how to load and process images.
+       Returns training and testing datasets. 
+       If retern_cls_counts=True, this is also returned as 3rd value.
     '''
     print("***********************************************************************")
     print("Load data:")
     print(f"  - Loading the dataset from: {path}.")
     print(f"  - Using a batch size of: {batch_size}.")
     print(f"  - Resizing input images to: {shape}.")
-    print(f"  - Returning class counts for later use? {return_cls_counts}")
+    print(f"  - Stratify when sampling? {stratify}")
+    print(f"  - Returning class counts to calculate wieghts? {return_cls_counts}")
     print("***********************************************************************")
-    # Define the directory path
-    directory_path = path
     
-    # Define the batch size
-    batch_size = batch_size
+    batch_size = batch_size  # Define the batch size
     
     # Define the image size using the 1st 2 elements of the shape parameter
     # We don't need the number of channels here, just the dimensions to use
     image_size = shape[:2]
+
+    # Get the class names
+    class_names = os.listdir(path)
+
+    images = [] # Initialize the images list
+    labels = [] # Initialize the labels list
+    cls_counts={}
+          
+    # Get the images and labels to use for training and validation
+    for class_name in class_names:
+        class_path = os.path.join(path, class_name)
+        for image_name in os.listdir(class_path):
+            image_path = os.path.join(class_path, image_name)
+            images.append(image_path)
+            labels.append(class_name)
+
+    # Print the number of number of images per class
+    print("\nFor the full dataset:")
+    for class_name in class_names:
+        print(f'Number of {class_name} images: {labels.count(class_name)}, or {labels.count(class_name)/len(labels)*100:.1f}%')
+        # Save class count to return if requested
+        cls_counts[class_name]=labels.count(class_name)
+        
+    # We originally used the image_dataset_from_directory function. That
+    # doesn't suppot stratification. So, we're changing to use a dataframe
+    # and an ImageDataGenerator.
+          
+    if stratify: # Use sklearn's train_test_split function to split the data 
+                 # into training and testing sets
+        # Split the data in a stratified manner
+        X_train, X_val, y_train, y_val = train_test_split(images, labels, 
+                                test_size=0.2, stratify=labels)
+    else:
+        # Split the data randomly
+        X_train, X_val = train_test_split(images, test_size=0.2)
+
+    # Build the DataFrames for the training and validation sets
+    train_df = pd.DataFrame(list(zip(X_train, y_train)), columns=['image', 'class'])
+    val_df = pd.DataFrame(list(zip(X_val, y_val)), columns=['image', 'class'])
     
-    # Load the dataset
-    X_train = tf.keras.preprocessing.image_dataset_from_directory(
-        directory_path,
-        batch_size=batch_size,
-        image_size=image_size,
-        validation_split=0.2,
-        subset='training',
-        seed=123,
-        labels='inferred',
-        label_mode='int'
-    )
+
+    # Define the ImageDataGenerator class with rescaling for each channel
+    # Normalizing the data is a good 1st step
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    val_datagen = ImageDataGenerator(rescale=1./255)
     
-    X_test = tf.keras.preprocessing.image_dataset_from_directory(
-        directory_path,
+    # Define the training and validation data generators
+    # Note that the training/validation split was already done above,
+    # so, we are not using the validation_split here.
+    train_generator = train_datagen.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='image',
+        y_col='class',
+        target_size=image_size,
         batch_size=batch_size,
-        image_size=image_size,
-        validation_split=0.2,
-        subset='validation',
-        seed=123,
-        labels='inferred',
-        label_mode='int'
-    )
+        class_mode='categorical',
+        shuffle=True)
+    
+    val_generator = val_datagen.flow_from_dataframe(
+        dataframe=val_df,
+        x_col='image',
+        y_col='class',
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True)
+
 
     if show_pictures:
         # Get the class names
-        class_names = X_train.class_names
+        class_names = list(train_generator.class_indices.keys())
         print(class_names)
 
         # Display up to 3 images from each of the categories
         for i, class_name in enumerate(class_names):
             plt.figure(figsize=(10, 10))
-            for images, labels in X_train.take(2):
-                images = images.numpy()
-                labels = labels.numpy()
-
-                # Filter images of the current class
-                class_images = images[labels == i]
-                
-                # Number of images to show.
-                # Limited by number of this class in the batch or specific number
-                num_images = min(len(class_images), 3)
-                
-                for j in range(num_images):
-                    ax = plt.subplot(1, num_images, j + 1)
-                    plt.imshow(class_images[j].astype("uint8"))
-                    plt.title(class_name)
-                    plt.axis("off")
-            plt.show()
             
-  
-    #Get and print counts by class
-    print("\nGetting number of images per class. This may take a bit...")
-    # Initialize counts
-    counts = [0] * len(X_train.class_names)
-    class_names=list(X_train.class_names)
+            # Get a single batch to use for display
+            images, labels = train_generator.next()
+
+            # Un-normalize the images for display
+            images = images * 255
+
+            # Filter images of the current class
+            class_images = images[labels[:, i] == 1]
+                
+            # Number of images to show. We don't want to show more than 3 images.
+            num_images = min(len(class_images), 3)
+                
+            for j in range(num_images):
+                ax = plt.subplot(1, num_images, j + 1)
+                plt.imshow(class_images[j].astype("uint8"))
+                plt.title(class_name)
+                plt.axis("off")
+            plt.show()
     
-    # Iterate through the training dataset batch by batch
-    for batch in X_train:
-        count_class(counts, batch, len(X_train.class_names))
-    
-    total=sum(counts)
-    
-    cls_counts={}
-    # Print the counts
-    print("\nFor the Training set:")
-    for i, count in enumerate(counts):
-        print(f"Category {class_names[i]}: {count} images or {count/total*100:.1f}% of total images.")
-        cls_counts[i]=count
-    
+    print("\nFor the training dataset:")         
+    for class_name  in class_names:
+        count = y_train.count(class_name)
+        print(f'Number of {class_name} images: {count}, or {count/len(y_train)*100:.1f}%')
+
     if return_cls_counts:
-        return X_train, X_test, cls_counts
+        print(f"\nClass counts being returned: {cls_counts}.")
+        return train_generator, val_generator, cls_counts
     else:
-        return X_train, X_test
+        return train_generator, val_generator
 
 def load_optimizer(optimizer_name):
     '''Takes an optimizer name as a string and checks if it's valid'''
@@ -190,23 +163,65 @@ def load_optimizer(optimizer_name):
         # Raise an exception if the optimizer name is invalid
         raise ValueError(f"Invalid optimizer name: {optimizer_name}")
 
+def make_model(activation='relu', shape=(80,80,3), num_classes=4):
+    '''Sets up a model. 
+          Takes in an activation function, shape for the input images, and number of classes.
+          Returns the model.'''
+    print("***********************************************************************")
+    print("Make model:")
+    print(f"  - Using the activation function: {activation}.")
+    print(f"  - Model will have {num_classes} classes.")
+    print("***********************************************************************")
+    
+    # Define the model
+    model = tf.keras.Sequential([
+        layers.Conv2D(32, (3, 3), padding='same', activation=activation, input_shape=shape),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), padding='same', activation=activation),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.2),
+        layers.Conv2D(128, (3, 3), padding='same', activation=activation),
+        layers.MaxPooling2D((2, 2), padding='same'),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation=activation),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    return model       
+        
 def compile_train_model(X_train, X_test, model,
-                        loss=SparseCategoricalCrossentropy(from_logits=True),
-                        optimizer='Adam', learning_rate=0.0001, epochs=10, weights=False, callbacks=[]):
+                        loss='categorical_crossentropy',
+                        optimizer='Adam', learning_rate=0.0001, epochs=10, cls_counts=False, log_name=None):
     '''Compiles and trains the model. 
           Takes in an X_train, X_test, model, loss function, optimizer, learning rate,
           epochs, if class weights should be used, and a list of callbacks.
           Returns the compiled model and training history.'''
     
-    num_classes = len(X_train.class_names)
+    # Deal with class weights
     
-    if weights:
-        # Calculate Class Weights to manage imballance in the dataset
-        print("Feature not yet implemented....sorry!")
+    num_classes = len(list(X_train.class_indices.keys()))
+    class_indices = range(num_classes)
+    
+    if not cls_counts:
+        cls_wt = [1] * num_classes # Create a list of 1s as long as number of classes
+        class_weights = dict(zip(class_indices, cls_wt))  
+    
     else:
-        weight_list = [1] * num_classes # Create a list of 1s as long as number of classes
-        class_indices = range(num_classes)
-        class_weight = dict(zip(class_indices, weight_list))  
+        # Calculate class weights to deal with imballance
+        class_names = list(X_train.class_indices.keys())
+        print(class_names)
+        # Make a y from cls_counts
+        y_vals = []
+        for cls in list(X_train.class_indices.keys()):
+            y_vals += [X_train.class_indices[cls]] * int(cls_counts[cls])
+
+        cls_wt = class_weight.compute_class_weight('balanced', 
+                                      classes=np.unique(y_vals), 
+                                      y=y_vals)
+
+        class_weights = dict(zip(class_indices, cls_wt))
+     
     
 
     print("***********************************************************************")
@@ -215,8 +230,7 @@ def compile_train_model(X_train, X_test, model,
     print(f"  - Using the optimizer: {optimizer}.")
     print(f"  - Using learning rate of: {learning_rate}.")
     print(f"  - Running for {epochs} epochs.")
-    print(f"   -Using class weights: {class_weight})")
-    print(f"  - Using these callbacks: {callbacks}")
+    print(f"   -Using class weights: {class_weights})")
     print("***********************************************************************")
     # Compile the model
     
@@ -225,24 +239,29 @@ def compile_train_model(X_train, X_test, model,
     model.compile(optimizer=opt,
                   loss=loss,
                   metrics=['accuracy'])
-    
+
+    time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    log_dir = f"logs/fit_{log_name}_{epochs}_{time}"
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 
     # Train the model
-    history = model.fit(X_train, epochs=epochs, validation_data=X_test, class_weight=class_weight, callbacks=[callbacks])
+    history = model.fit(X_train, epochs=epochs, validation_data=X_test, class_weight=class_weights, callbacks=[tensorboard_callback])
     
     return model, history
 
-def evaluate_model(X_train, X_test, model, history, num_classes=4):
+
+def evaluate_model(train_set, val_set, model, history, num_classes=4):
     '''Evaluates a model. 
-          Takes in an X_train, X_test, model, history, number of classes.'''
+          Takes in a train_set, val_set, model, history, number of classes.'''
     print("***********************************************************************")
     print("Evaluate the model:")
     print("***********************************************************************")
     # Evaluate the model
-    loss, accuracy = model.evaluate(X_test)
+    loss, accuracy = model.evaluate(train_set)
     print(f'Test loss: {loss}')
     print(f'Test accuracy: {accuracy}')
-
 
     # Plot the training and validation loss over time
     plt.plot(history.history['loss'], label='Training Loss')
@@ -262,17 +281,22 @@ def evaluate_model(X_train, X_test, model, history, num_classes=4):
     plt.legend()
     plt.show()
 
-    # Get the class names
-    class_names = X_test.class_names
+    class_names = list(train_set.class_indices.keys()) # Get the class names
+    
+    y_pred = np.argmax(model.predict(val_set), axis=-1) # Make predictions on the test set
 
-    # Make predictions on the test set
-    y_pred = np.argmax(model.predict(X_test), axis=-1)
-    
+    num_val_samples = val_set.samples  # total number of validation samples
+    batch_size = val_set.batch_size  # batch size
+
+    # Calculate the number of batches
+    num_batches = np.ceil(num_val_samples / batch_size)
+    num_batches = int(num_batches)
+
     # Get the true labels
-    y_true = np.concatenate([y for x, y in X_test], axis=0)
-    
+    y_true = np.concatenate([y for x, y in (next(val_set) for _ in range(num_batches))], axis=0)
+
     # Compute the confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
+    cm = confusion_matrix(y_true.argmax(axis=1), y_pred)
     
     # Plot the confusion matrix
     plt.imshow(cm, cmap=plt.cm.Blues)
@@ -281,7 +305,7 @@ def evaluate_model(X_train, X_test, model, history, num_classes=4):
     plt.ylabel('True Label')
     plt.xticks(range(num_classes),class_names)
     plt.yticks(range(num_classes), class_names)
-    plt.colorbar()
+    # plt.colorbar()
     for i in range(num_classes):
         for j in range(num_classes):
             plt.text(j, i, cm[i, j], ha='center', va='center', color='black')
